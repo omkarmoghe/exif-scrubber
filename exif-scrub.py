c28bc95c8fbf4128e61ea840b85b7776ed01b709
Omkar Moghe <omoghe@umich.edu>
2020-05-31 19:41:19 -0700

    ada

--- exif-scrub.py
+++ exif-scrub.py
@@ -7,16 +7,16 @@ import os
 
 @click.command()
 @click.argument('file_glob', type=click.STRING)
-@click.option('-G', '--gps', type=click.BOOL, default=False)
-@click.option('-A', '--all', type=click.BOOL, default=False)
+@click.option('-G', '--gps', is_flag=True)
+@click.option('-A', '--all', is_flag=True)
 def exif_scrub(file_glob, gps, all):
   filepaths = glob.glob(file_glob)
-  modified = []
+  scrubbed = {}
 
   for filepath in filepaths:
     with open(filepath, 'rb') as image_file:
       image = exif.Image(image_file)
-      if !image.has_exif:
+      if not image.has_exif:
         continue
 
       # Remove EXIF data.
@@ -28,8 +28,11 @@ def exif_scrub(file_glob, gps, all):
 
       # Write new image file.
       filename, extension = os.path.splitext(filepath)
-      with open(f"{filename}_{get_postfix()}.{extension}", 'wb') as new_image_file:
-        new_image_file.write(image.get_file())
+      scrubbed[f"{filename}_SCRUBBED{extension}"] = image.get_file()
+
+  for name, image_bytes in scrubbed.items():
+    with open(name, 'wb') as new_image_file:
+      new_image_file.write(image_bytes)
 
 def remove_gps(image):
   gps_attrs = [e_attr for e_attr in dir(image) if re.match('gps', e_attr)]
@@ -41,9 +44,6 @@ def remove_all(image):
 def remove_exif_attrs(image, exif_attrs):
   for exif_attr in exif_attrs:
     delattr(image, exif_attr)
-
-def get_postfix():
-  f"SCRUBBED_{datetime.datetime.now().replace(microsecond=0).isoformat()}"
 
 if __name__ == '__main__':
   exif_scrub()
